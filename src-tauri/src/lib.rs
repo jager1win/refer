15b2d060231b44157fa9f30e9c44710e217a894e
Yuriy (Jager) Lysov <jagerwin@ya.ru>
2026-01-18 16:17:56 +0300

    create Settings(theme,lang)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,230 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
+use std::sync::Mutex;
+use tauri::Wry;
+use tauri::{Builder, Manager};
+use tauri_plugin_store::StoreExt;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct SettingsStore {
+    pub theme: String,
+    pub language: String,
 }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
+impl Default for SettingsStore {
+    fn default() -> Self {
+        Self {
+            theme: "light".into(),
+            language: "en".into(),
+        }
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+struct AppStatistics {
+    pub settings_path: PathBuf,
+    pub db_path: PathBuf,
+    pub db_path_size: u32,
+    pub db_count: u32,
+    pub db_names: Vec<String>,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct AppDb {
+    pub db_path: PathBuf,
+}
+
+// universal path to config
+fn settings_path(app: &tauri::AppHandle) -> PathBuf {
+    #[cfg(target_os = "android")]
+    {
+        // Android
+        let mut path = app.path().app_local_data_dir().unwrap();
+        path.push("settings.json");
+        path
+    }
+    #[cfg(not(target_os = "android"))]
+    {
+        // Linux/Windows
+        let mut path: PathBuf = app.path().app_config_dir().unwrap();
+        path.push("settings.json");
+        path
+    }
+}
+
+fn load_settings(app: &tauri::AppHandle) -> SettingsStore {
+    let path = settings_path(app);
+    match fs::read_to_string(&path) {
+        Ok(s) => serde_json::from_str::<SettingsStore>(&s).unwrap_or_default(),
+        Err(_) => {
+            // файл не найден — дефолт
+            SettingsStore::default()
+        }
+    }
+}
+
+fn save_settings(app: &tauri::AppHandle, state: &SettingsStore) {
+    if let Ok(s) = serde_json::to_string_pretty(state) {
+        /*if let Some(dir) = app.path().app_dir() {
+            // создаём папку, если нужно
+            let _ = std::fs::create_dir_all(&dir);
+        }*/
+        let _ = fs::write(settings_path(app), s); // игнорируем ошибку записи (можно логировать)
+    }
+}
+
+/*#[tauri::command]
+fn get_settings(state: tauri::State<'_, Mutex<SettingsState>>) -> SettingsState {
+    state.lock().unwrap().clone()
+}
+
+#[tauri::command]
+fn set_settings(
+    new: SettingsState,
+    state: tauri::State<'_, Mutex<SettingsState>>,
+    app: tauri::AppHandle,
+) {
+    {
+        let mut guard = state.lock().unwrap();
+        *guard = new.clone();
+    }
+    // асинхронно сохранить — тут просто синхронно (недолго для двух полей)
+    save_settings(&app, &new);
+}*/
+
+#[tauri::command]
+async fn get_settings(app: tauri::AppHandle) -> Result<SettingsStore, String> {
+    let store = app.store(".settings.json").map_err(|e| e.to_string())?;
+
+    println!("{:?}",store.get("theme"));
+
+    // Читаем настройки или возвращаем дефолтные
+    let theme = match store.get("theme") {
+        Some(value) => value.as_str().unwrap_or("light").to_string(),
+        None => {store.set("theme", "light");"light".into()},
+    };
+//
+    let language = match store.get("language") {
+        Some(value) => value.as_str().unwrap_or("en").to_string(),
+        None => {store.set("language", "en");"en".into()},
+    };
+
+    match store.save() {
+        Ok(_) => {
+            println!("Settings saved successfully");
+        },
+        Err(e) => {
+            println!("Failed to save: {}", e);
+        }
+    }
+
+    Ok(SettingsStore { theme, language })
+}
+
+#[tauri::command]
+async fn set_settings(app: tauri::AppHandle, new: SettingsStore) -> Result<(), String> {
+    let store = app.store(".settings.json").map_err(|e| e.to_string())?;
+
+    store.set("theme", new.theme);
+    store.set("language", new.language);
+    store.save().map_err(|e| e.to_string())
+}
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+/*#[tauri::command]
+async fn get_settings(app: tauri::AppHandle) -> Result<AppState, String> {
+    let app_dir = get_settings_path(&app);
+
+    let settings_path = app_dir.join("settings.json");
+
+    let mut default_settings = serde_json::json!({
+        "theme": "light",
+        "language": "en",
+    });
+
+    if !settings_path.exists() {
+        return create_default_settings(&app_dir, &settings_path, default_settings);
+    }
+
+    match std::fs::read_to_string(&settings_path) {
+        Ok(content) => {
+            match serde_json::from_str::<serde_json::Value>(&content) {
+                Ok(settings) => {
+                    Ok(settings)
+                }
+                Err(_e) => {
+                    create_default_settings(&app_dir, &settings_path, default_settings)
+                }
+            }
+        }
+        Err(_e) => {
+            create_default_settings(&app_dir, &settings_path, default_settings)
+        }
+    }
+}
+
+#[tauri::command]
+async fn save_settings(app: tauri::AppHandle, settings: Value) -> Result<(), String> {
+    let path = get_settings_path(&app);
+
+    // Создать директорию если нужно
+    if let Some(parent) = path.parent() {
+        fs::create_dir_all(parent).map_err(|e| format!("Failed to create directory: {}", e))?;
+    }
+
+    let content = serde_json::to_string_pretty(&settings)
+        .map_err(|e| format!("Failed to serialize settings: {}", e))?;
+
+    fs::write(path, content).map_err(|e| format!("Failed to write settings: {}", e))
+}
+
+#[tauri::command]
+async fn save_settings(settings: serde_json::Value, app: tauri::AppHandle) -> Result<(), String> {
+    let settings_path = get_settings_path(&app);
+
+    // Сохраняем настройки
+    std::fs::write(
+        &settings_path,
+        serde_json::to_string_pretty(&settings).unwrap()
+    ).map_err(|e| format!("Не удалось сохранить настройки: {}", e))?;
+
+    println!("Настройки сохранены в: {:?}", settings_path);
+    Ok(())
+}*/
+
+//#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    Builder::default()
+        .setup(|app| {
+            // читаем настройки (или берем дефолт)
+            ////let initial = load_settings(app.handle());
+            // сохраняем управляемое состояние: Mutex<AppState>
+            ////app.manage(Mutex::new(initial));
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .invoke_handler(tauri::generate_handler![get_settings, set_settings])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+/*
+fn create_default_settings(
+    app_dir: &std::path::Path,
+    settings_path: &std::path::Path,
+    default_settings: serde_json::Value
+) -> Result<serde_json::Value, String> {
+    std::fs::create_dir_all(app_dir)
+        .map_err(|e| format!("Failed to create directory: {}", e))?;
+
+    std::fs::write(
+        settings_path,
+        serde_json::to_string_pretty(&default_settings).unwrap()
+    ).map_err(|e| format!("Failed to create settings.json: {}", e))?;
+
+    Ok(default_settings)
+}*/
--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,11 @@
 use leptos::task::spawn_local;
 use leptos::{ev::SubmitEvent, prelude::*};
 use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
+
+include!(concat!(env!("OUT_DIR"), "/i18n/mod.rs"));
+use i18n::*;
 
 #[wasm_bindgen]
 extern "C" {
@@ -9,54 +13,167 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
-#[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct AppSettings {
+    theme: String,
+    language: String,
+}
+
+#[derive(Serialize)]
+struct SettingsBack {
+    new: AppSettings,
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (name, set_name) = signal(String::new());
-    let (greet_msg, set_greet_msg) = signal(String::new());
+    leptos_meta::provide_meta_context();
+    let (settings, set_settings) = signal::<AppSettings>(AppSettings {
+        theme: "dark".into(),
+        language: "ru".into(),
+    });
+    let (status, set_status) = signal(String::from(""));
 
-    let update_name = move |ev| {
-        let v = event_target_value(&ev);
-        set_name.set(v);
-    };
+    let active_tab = RwSignal::new(1);
 
-    let greet = move |ev: SubmitEvent| {
-        ev.prevent_default();
+    // init settings
+    spawn_local(async move {
+        let js = invoke("get_settings", JsValue::NULL).await;
+        match from_value::<AppSettings>(js) {
+            Ok(s) => set_settings.set(s),
+            Err(e) => set_status.set(format!("deserialize failed: {}", e)),
+        }
+    });
+    Effect::new(move |_| {
+        settings.track();
         spawn_local(async move {
-            let name = name.get_untracked();
-            if name.is_empty() {
-                return;
-            }
+            let args = to_value(&SettingsBack {new: settings.get()}).unwrap();
+            let _ = invoke("set_settings", args).await;
+        });
+    });
+    //log::debug!("act: {:?}", settings.get());
+    view! {
+        <I18nContextProvider>
+            <nav class="top-nav">
+                <button
+                    class:active={move || active_tab.get() == 0}
+                    on:click={move |_| active_tab.set(0)}
+                >"⚙"</button>
+                <button
+                    class:active={move || active_tab.get() == 1}
+                    on:click={move |_| active_tab.set(1)}
+                >{let i18n = use_i18n();t!(i18n, nav.references)} </button>
+                <button
+                    class:active={move || active_tab.get() == 2}
+                    on:click={move |_| active_tab.set(2)}
+                >"📥"</button>
+            </nav>
+            <main class="container">
+                <div class="tab-content" class:active={move || active_tab.get() == 0}>
+                    <Settings getter=settings setter=set_settings/>
+                </div>
+                <div class="tab-content" class:active={move || active_tab.get() == 1}>
+                    <References />
+                </div>
+                <div class="tab-content" class:active={move || active_tab.get() == 2}>
+                    <Import />
+                </div>
+            </main>
+        </I18nContextProvider>
+    }
+}
 
-            let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &name }).unwrap();
-            // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-            let new_msg = invoke("greet", args).await.as_string().unwrap();
-            set_greet_msg.set(new_msg);
+#[component]
+fn Settings(getter: ReadSignal<AppSettings>, setter: WriteSignal<AppSettings>) -> impl IntoView {
+    let i18n = use_i18n();
+    let all: &[Locale] = Locale::get_all();
+    let backend_locale = getter.get().language.trim().to_lowercase();
+
+    // Найти локаль по 2‑буквенному коду (с учетом возможных вариаций)
+    let selected = all
+        .iter()
+        .find(|l| l.to_string() == backend_locale)
+        .or_else(|| all.iter().find(|l| l.to_string() == "en"))
+        .or(all.first());
+
+    if let Some(&loc) = selected {
+        i18n.set_locale(loc);
+    }
+
+    //let current = move || i18n.get_locale();
+    let toggle_theme = move |_| {
+        setter.update(|current| {
+            if current.theme == "light" {
+                current.theme = "dark".to_string();
+            } else {
+                current.theme = "light".to_string();
+            }
         });
     };
 
+    Effect::new(move |_| {
+        let theme_value = getter.get().theme;
+        let document = window().document().unwrap();
+        let html_element = document.document_element().unwrap();
+
+        html_element
+            .set_attribute("data-theme", &theme_value)
+            .unwrap();
+        html_element.set_attribute("class", &theme_value).unwrap();
+    });
+
+    //log::debug!("lang: {:?}", &all);
+    //log::debug!("lang: {:?}", &current);
     view! {
-        <main class="container">
-            <h1>"Welcome to Tauri + Leptos"</h1>
+        <h2>{t!(i18n, settings.title)}</h2>
 
-            <div class="row">
-                "row"
-            </div>
-            <p>"Click on the Tauri and Leptos logos to learn more."</p>
+        <div class="locale-switcher">
+            {all.iter().map(move |&loc| {
+                let code = loc.as_str();
+                let is_active = move || getter.get().language == loc.to_string();
+                view! {
+                    <button
+                        class=move || if is_active() { "locale-btn active".to_string() } else { "locale-btn".to_string() }
+                        on:click=move |_| {
+                            spawn_local(async move {
+                                i18n.set_locale(loc);
+                                setter.update(|current|{
+                                    current.language = loc.as_str().to_string();
+                                })
+                            });
+                        }
+                    >
+                        {code}
+                    </button>
+                }
+            }).collect_view()}
+        </div>
 
-            <form class="row" on:submit=greet>
-                <input
-                    id="greet-input"
-                    placeholder="Enter a name..."
-                    on:input=update_name
-                />
-                <button type="submit">"Greet"</button>
-            </form>
-            <p>{ move || greet_msg.get() }</p>
-        </main>
+        <h2>{t!(i18n, theme.title)}</h2>
+
+        <button on:click=toggle_theme class="theme-switcher" >
+            {move || match getter.get().theme.as_str() {
+                "light" => "🌙",
+                "dark" => "🌞",
+                _ => "🌞",
+            }}
+        </button>
+    }
+}
+
+#[component]
+fn References() -> impl IntoView {
+    let i18n = use_i18n();
+
+    view! {
+            <h2>234234</h2>
+    }
+}
+
+#[component]
+fn Import() -> impl IntoView {
+    let i18n = use_i18n();
+
+    view! {
+            <h2>{t!(i18n, import.title)}</h2>
     }
 }
